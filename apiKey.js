ee7bda3dbf73d2a4c9e97b7502bad6c15ed2228d
Sanja <gamesg274@gmail.com>
2023-01-12 23:53:54 +0100

    Added file for apiKey with instruction

---
+++ apiKey.js
@@ -0,0 +1,16 @@
+/*
+  How to get api key:
+  Go to: https://developers.giphy.com/
+  Register
+  Go to your account/dashboard
+  Click to create new project ( it will just generate free key for you)
+  Copy key and paste in place of "Your_api_key", format of key is random letters/numbers
+
+  You can also follow instruction from:
+  https://developers.giphy.com/docs/api#quick-start-guide
+*/
+
+// should look like this:
+// export default "skjFUI4654"
+
+export default "Your_api_key";
